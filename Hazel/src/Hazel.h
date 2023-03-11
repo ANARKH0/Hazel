48d0e244a1dae6290b8fad6af785623cea4b2370
ANARKH0 <anarkh_hp@163.com>
2023-03-11 18:26:46 +0800

    Setup Log reference for spdlog.

--- Hazel/src/Hazel.h
+++ Hazel/src/Hazel.h
@@ -2,9 +2,9 @@
 
 // For use by Hazel applications
 
-#include <stdio.h>
 #include "Hazel/Application.h"
-
+#include "Hazel/Log.h"
+#include "Hazel/Log.cpp"
 //-----Entry Point----------------------
 #include "Hazel/EntryPoint.h"
 //--------------------------------------
---
+++ Hazel/src/Hazel/Log.cpp
@@ -0,0 +1,20 @@
+#include "Log.h"
+#include "spdlog/sinks/stdout_color_sinks.h"
+
+namespace Hazel {
+
+	//template class HAZEL_API std::shared_ptr<spdlog::logger>;
+
+	std::shared_ptr<spdlog::logger> Log::s_CoreLogger;		//log for engine
+	std::shared_ptr<spdlog::logger> Log::s_ClientLogger;		//log for client
+
+    void Log::Init() {
+		spdlog::set_pattern("%^[%T] %n: %v%$"); // 时间戳和颜色
+
+		s_CoreLogger = spdlog::stdout_color_mt("HAZEL"); //mt 多线程缩写
+		s_CoreLogger->set_level(spdlog::level::trace);  // trace 跟踪 值0
+
+		s_ClientLogger = spdlog::stdout_color_mt("APP");
+		s_ClientLogger->set_level(spdlog::level::trace);
+	}
+}
